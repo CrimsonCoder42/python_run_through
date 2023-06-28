39f59e0a49e8dc23c54f6ba3f6e523e9b67c1c24
CrimsonCoder42 <nostro37@gmail.com>
2023-06-28 09:20:15 -0400

    completed day 22.

--- snake_game/main_snake.py
+++ snake_game/main_snake.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake_game.snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 
@@ -10,7 +12,10 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 
-snake = Snake(0, 0)
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(key="Up", fun=snake.up)
 screen.onkey(key="Down", fun=snake.down)
@@ -20,11 +25,23 @@ screen.onkey(key="Right", fun=snake.right)
 
 game_is_on = True
 while game_is_on:
-    snake.snake_maker()
     screen.update()
     time.sleep(0.1)
     snake.move()
 
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.add_segment()
+        scoreboard.increase_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.snake[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
+
--- snake_game/snake.py
+++ snake_game/snake.py
@@ -8,16 +8,19 @@ RIGHT = 0
 
 
 class Snake:
-    def __init__(self, x_axis, y_axis):
+    def __init__(self):
         self.snake = []
-        self.x_axis = x_axis
-        self.y_axis = y_axis
+        self.x_axis = 0
+        self.y_axis = 0
+        self.snake_maker()
+        self.head = self.snake[0]
+        self.tail = self.snake[-1]
 
-    def snake_add(self, x_axis, y_axis):
+    def snake_add(self, x_spot, y_spot):
         new_section = Turtle(shape="square")
         new_section.penup()
         new_section.color("white")
-        new_section.goto(x=x_axis, y=y_axis)
+        new_section.goto(x=x_spot, y=y_spot)
         self.snake.append(new_section)
 
     def snake_maker(self):
@@ -26,12 +29,19 @@ class Snake:
                 location = _ * -20
                 self.snake_add(location, 0)
 
+    def add_segment(self):
+        last_seg = self.snake[-1]
+        x = last_seg.xcor()
+        y = last_seg.ycor()
+        self.snake_add(x, y)
+
     def move(self):
         for seg_num in range(len(self.snake) - 1, 0, -1):
             new_x = self.snake[seg_num - 1].xcor()
             new_y = self.snake[seg_num - 1].ycor()
             self.snake[seg_num].goto(new_x, new_y)
         self.snake[0].forward(MOVE_DISTANCE)
+
 
     def right(self):
         if self.snake[0].heading() != LEFT:
